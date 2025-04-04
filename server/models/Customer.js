23a295da06d78ff6155ddaa0cd0fc01e080112c3
Matt Thomas <matsinet@gmail.com>
2025-04-03 20:01:26 -0500

    Added customer controller and model for review

---
+++ server/models/Customer.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z ]*$/
+  },
+  phone: {
+    type: Number,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  },
+  address: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9,.' ]*$/
+  },
+  city: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9,.' ]*$/
+  },
+  state: {
+    type: String,
+    required: true,
+    validate: /^[A-Za-z0-9,.' ]*$/,
+    minLength: 2,
+    maxLength: 2
+  },
+  zipcode: {
+    type: String,
+    required: true,
+    validate: /^[0-9]*$/,
+    minLength: 5,
+    maxLength: 5
+  }
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;

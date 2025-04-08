8baee8987dfcb5b3cb444c34ab4dc82dde77607e
Matt Thomas <matsinet@gmail.com>
2025-04-07 19:20:23 -0500

    Lesson 9.1 form complete

--- server/controllers/pizzas.js
+++ server/controllers/pizzas.js
@@ -68,4 +68,37 @@ router.delete("/:id", async (request, response) => {
   }
 });
 
+// Update a single pizza by ID
+router.put("/:id", async (request, response) => {
+  try {
+    const body = request.body;
+
+    const data = await Pizza.findByIdAndUpdate(
+      request.params.id,
+      {
+        $set: {
+          crust: body.crust,
+          cheese: body.cheese,
+          sauce: body.sauce,
+          toppings: body.toppings
+        }
+      },
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    response.json(data);
+  } catch (error) {
+    // Output error to the console incase it fails to send in response
+    console.log(error);
+
+    if ("name" in error && error.name === "ValidationError")
+      return response.status(400).json(error.errors);
+
+    return response.status(500).json(error.errors);
+  }
+});
+
 export default router;
